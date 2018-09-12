f9172d15b11072c807b520ab607aabf8005c866c
Vanille <vanille@twistudio.fr>
2018-09-12 11:31:34 +0200

    Mes livres (grille) - Ajout de la partie "Ajouter une sélection"

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ $(".regular").slick({
     accessibility:true
 });
 
+
+var ajout_selection=0;
+
+$(".meslivres_ajouter").click(function(){
+	if (ajout_selection===0) {
+		$("form#tag_recherche").css("display","inline-block");
+		ajout_selection = 1;
+	}
+
+	else{
+		$("form#tag_recherche").css("display","none");
+		ajout_selection = 0;
+	}
+});
